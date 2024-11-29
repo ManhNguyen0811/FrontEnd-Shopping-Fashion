aeaafe5ee5c39bb2a1188ff2944efad9f8eb80b6
TranThanhDat2407 <tranthanhdat2407.email@gmail.com>
2024-11-29 15:14:07 +0700

    fix conflix 2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,12 +5,9 @@ import { ProductsComponent } from './components/products/products.component';
 import { DetailProductComponent } from './components/detail-product/detail-product.component';
 import { LoginComponent } from './components/login/login.component';
 import { SigninComponent } from './components/signin/signin.component';
-// @ts-ignore
-import { ForgotPassComponent } from './components/forgot-pass/forgot-pass.component';
+import { ForgotPasswordComponent} from './components/forgot-password/forgot-password.component';
 import { CartComponent } from './components/cart/cart.component';
 import {PaymentsComponent} from './components/payments/payments.component';
-import {AddressListComponent} from './components/address-list/address-list.component';
-import {ProfileComponent} from './components/profile/profile.component';
 import {WishlistComponent} from './components/wishlist/wishlist.component';
 
 
@@ -21,7 +18,7 @@ export const routes: Routes = [
      { path: 'detail_product', component: DetailProductComponent },
      { path: 'login', component: LoginComponent },
      { path: 'signin', component: SigninComponent },
-     { path: 'forgotPass', component: ForgotPassComponent },
+     { path: 'forgotPass', component: ForgotPasswordComponent },
      { path: 'cart', component: CartComponent },
      { path: 'wishlist', component: WishlistComponent },
      { path: 'payments', component: PaymentsComponent },
